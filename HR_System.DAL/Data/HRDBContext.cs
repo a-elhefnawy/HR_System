bdf1db16ce902356e99431449722251d99aedda5
a-elhefnawy <abdelrhman.elhefnawy97@gmail.com>
2023-06-19 22:54:43 +0300

    Add project files.

---
+++ HR_System.DAL/Data/HRDBContext.cs
@@ -0,0 +1,19 @@
+﻿using HR_System.DAL.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR_System.DAL.Data
+{
+    public class HRDBContext:IdentityDbContext<AppUser>
+    {
+        public HRDBContext(DbContextOptions options):base(options)
+        {
+            
+        }
+    }
+}
