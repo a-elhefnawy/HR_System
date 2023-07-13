3001a76f645a103e22007adebc8fb08d3773d69a
a-elhefnawy <abdelrhman.elhefnawy97@gmail.com>
2023-07-13 10:48:30 +0300

    PreFinalPush

--- HR_System.PAL/Controllers/HomeController.cs
+++ HR_System.PAL/Controllers/HomeController.cs
@@ -1,9 +1,11 @@
 ﻿using HR_System.PAL.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
 namespace HR_System.PAL.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
